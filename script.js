ce16a20c5f5ceb2ebc70420b9b6bf95279a36a47
oolawo <oolawo@users.noreply.github.com>
2020-07-24 20:32:28 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.body.onkeyup = function(e){
+  if(e.keyCode== 68){
+    document.body.classList.toggle("dark");//.toggle toggles a specific class within the css or creates it if it doesn't exist
+  }
+ 
+}
